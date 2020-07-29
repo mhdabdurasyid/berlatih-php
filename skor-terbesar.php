438ab2219bab5e3c0a09296e013dcf6631873d05
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-07-29 20:10:42 +0700

    code utk jawaban soal 9

--- skor-terbesar.php
+++ skor-terbesar.php
@@ -2,40 +2,47 @@
 
 function skor_terbesar($arr)
 {
-    //kode di sini
-
+  //kode di sini
+  $nilai = array();
+  foreach ($arr as $i => $value) {
+    $nilai[$i] = $value["nilai"];
+  }
+  array_multisort($nilai, SORT_DESC, $arr);
+  return $arr;
 }
 
 // TEST CASES
 $skor = [
-    [
-        "nama" => "Bobby",
-        "kelas" => "Laravel",
-        "nilai" => 78
-    ],
-    [
-        "nama" => "Regi",
-        "kelas" => "React Native",
-        "nilai" => 86
-    ],
-    [
-        "nama" => "Aghnat",
-        "kelas" => "Laravel",
-        "nilai" => 90
-    ],
-    [
-        "nama" => "Indra",
-        "kelas" => "React JS",
-        "nilai" => 85
-    ],
-    [
-        "nama" => "Yoga",
-        "kelas" => "React Native",
-        "nilai" => 77
-    ],
+  [
+    "nama" => "Bobby",
+    "kelas" => "Laravel",
+    "nilai" => 78
+  ],
+  [
+    "nama" => "Regi",
+    "kelas" => "React Native",
+    "nilai" => 86
+  ],
+  [
+    "nama" => "Aghnat",
+    "kelas" => "Laravel",
+    "nilai" => 90
+  ],
+  [
+    "nama" => "Indra",
+    "kelas" => "React JS",
+    "nilai" => 85
+  ],
+  [
+    "nama" => "Yoga",
+    "kelas" => "React Native",
+    "nilai" => 77
+  ],
 ];
 
+echo "<pre>";
 print_r(skor_terbesar($skor));
+echo "</pre>";
 /* OUTPUT
   Array (
     [Laravel] => Array
