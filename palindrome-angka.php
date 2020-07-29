67bf1661e752f424b51f2a51e32b6d8b796609c1
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-07-29 13:21:07 +0700

    buat soal no 1

---
+++ palindrome-angka.php
@@ -0,0 +1,13 @@
+<?php
+
+function palindrome_angka($angka) {
+  // tulis kode di sini
+  echo "Holla";
+}
+
+// TEST CASES
+echo palindrome_angka(8); // 9
+echo palindrome_angka(10); // 11
+echo palindrome_angka(117); // 121
+echo palindrome_angka(175); // 181
+echo palindrome_angka(1000); // 1001
