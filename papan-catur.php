db6a05e508d40f31bad1cc1d44ba7b35fa1f248c
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-07-29 14:09:11 +0700

    jawaban soal 2

--- papan-catur.php
+++ papan-catur.php
@@ -3,6 +3,18 @@
 function papan_catur($angka)
 {
     // tulis kode di sini
+    echo "<pre>";
+    for ($i = 0; $i < $angka; $i++) {
+        for ($j = 0; $j < $angka; $j++) {
+            if ($i % 2 == 0) {
+                echo "# ";
+            } else if ($i % 2 == 1 && $j < $angka - 1) {
+                echo " #";
+            }
+        }
+        echo "<br>";
+    }
+    echo "</pre>";
 }
 
 // TEST CASES
