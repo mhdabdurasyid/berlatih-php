5aebd2898685444c7bcb378a652d9847a18533dd
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-07-29 14:51:48 +0700

    jawaban soal

---
+++ cari-mean.php
@@ -0,0 +1,18 @@
+<?php
+
+function cari_mean($arr)
+{
+    $total = 0;
+    for ($i = 0; $i < count($arr); $i++) {
+        $total += $arr[$i];
+    }
+    $mean = round($total / count($arr));
+    echo "$mean<br>";
+}
+
+//TEST CASE 
+echo cari_mean([1, 2, 3, 4, 5]); // 3
+echo cari_mean([3, 5, 7, 5, 3]); // 5
+echo cari_mean([6, 5, 4, 7, 3]); // 5
+echo cari_mean([1, 3, 3]); // 2
+echo cari_mean([7, 7, 7, 7, 7]); // 7
---
+++ pasangan-terbesar.php
@@ -0,0 +1,20 @@
+<?php
+function pasangan_terbesar($angka)
+{
+    // kode di sini
+    $max = 0;
+    $angka = (string)$angka;
+    for ($i = 0; $i < strlen($angka); $i++) {
+        if ((int)substr($angka, $i, 2) > $max) {
+            $max = substr($angka, $i, 2);
+        }
+    }
+    echo "$angka -> $max<br>";
+}
+
+// TEST CASES
+echo pasangan_terbesar(641573); // 73
+echo pasangan_terbesar(12783456); // 83
+echo pasangan_terbesar(910233); // 91
+echo pasangan_terbesar(71856421); // 85
+echo pasangan_terbesar(79918293); // 99
